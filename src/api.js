7ed6a4038e35ce4ef0b0dcd274778d479215e682
daichi-sato-design <daichisato.design@gmail.com>
2022-04-04 13:14:48 +0900

    JWTトークンをリクエストに追加

--- src/api.js
+++ src/api.js
@@ -1,10 +1,25 @@
 import axios from 'axios';
+import { logout } from './shared/utils/auth';
 
 const apiClient = axios.create({
   baseURL: 'http://localhost:5002/api',
   timeout: 1000,
 });
+apiClient.interceptors.request.use(
+  (config) => {
+    const userDetails = localStorage.getItem('user');
+    if (userDetails) {
+      const token = JSON.parse(userDetails).token;
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
 
+// public toutes
 export const login = async (data) => {
   try {
     return await apiClient.post('/auth/login', data);
@@ -15,7 +30,6 @@ export const login = async (data) => {
     };
   }
 };
-
 export const register = async (data) => {
   try {
     return await apiClient.post('/auth/register', data);
@@ -24,5 +38,14 @@ export const register = async (data) => {
       error: true,
       exception,
     };
+  }
+};
+
+// secure toutes
+
+const checkResponseCode = (exception) => {
+  const responseCode = exception?.response?.status;
+  if (responseCode) {
+    (responseCode === 401 || responseCode === 403) && logout();
   }
 };
