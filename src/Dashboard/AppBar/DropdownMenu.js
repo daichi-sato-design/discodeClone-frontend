2ae6c7c04f35f1e18c38292bc6069d9df02488e0
daichi-sato-design <daichisato.design@gmail.com>
2022-04-09 22:01:33 +0900

    ログアウトのドロップダウンメニューを作成

---
+++ src/Dashboard/AppBar/DropdownMenu.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { IconButton, Menu, MenuItem } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { logout } from '../../shared/utils/auth';
+
+export default function BasicMenu() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton onClick={handleMenuOpen} style={{ color: '#FFF' }}>
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleMenuClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={logout} style={{ fontSize: '14px' }}>
+          ログアウト
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
