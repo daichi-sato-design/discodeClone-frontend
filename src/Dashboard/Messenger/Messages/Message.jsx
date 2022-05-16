291c5307ab50ef666aee2033680a8e298cfb9dac
daichi-sato-design <daichisato.design@gmail.com>
2022-05-16 23:03:55 +0900

    チャットメッセージの作成

---
+++ src/Dashboard/Messenger/Messages/Message.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { styled } from '@mui/system';
+import Avater from '../../../shared/components/Avatar';
+import { Typography } from '@mui/material';
+
+const MainContainer = styled('div')({
+  width: '97%',
+  display: 'flex',
+  marginTop: '10px',
+});
+
+const AvatarContainer = styled('div')({
+  width: '70px',
+});
+
+const MessageContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+const MessageContent = styled('div')({
+  color: '#DCDDDE',
+});
+
+const SameAuthorMessageContent = styled('div')({
+  color: '#DCDDDE',
+  width: '97%',
+});
+
+const SameAuthorMessageText = styled('span')({
+  marginLeft: '70px',
+});
+
+const Message = ({ content, username, sameAuthor, date, sameDay }) => {
+  if (sameAuthor && sameDay) {
+    return (
+      <SameAuthorMessageContent>
+        <SameAuthorMessageText>{content}</SameAuthorMessageText>
+      </SameAuthorMessageContent>
+    );
+  }
+  return (
+    <MainContainer>
+      <AvatarContainer>
+        <Avater username={username} />
+      </AvatarContainer>
+      <MessageContainer>
+        <Typography style={{ fontSize: '16px', color: '#FFF' }}>
+          {username}{' '}
+          <span style={{ fontSize: '12px', color: '#72767D' }}>{date}</span>
+        </Typography>
+        <MessageContent>{content}</MessageContent>
+      </MessageContainer>
+    </MainContainer>
+  );
+};
+
+export default Message;
